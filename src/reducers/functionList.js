d272145b44710781a40ef70b82d712b78efff917
Andrew Gracey <agracey@gmail.com>
2019-07-04 07:11:34 -0700

    reformatting if statements

--- src/reducers/functionList.js
+++ src/reducers/functionList.js
@@ -1,12 +1,13 @@
 
 export function functionListReducer (state = {}, action) {
 
-  if(action.type ==='FUNCTION_LIST_LOAD' & action.status === 'SUCCESS' ) 
-    return Object.assign({}, state, { list: action.functionList, status: action.status })
+  if(action.type === 'FUNCTION_LIST_LOAD'){
+    if(action.status === 'SUCCESS' ) 
+      return Object.assign({}, state, { list: action.functionList, status: action.status })
 
-  if(action.type ==='FUNCTION_LIST_LOAD' & action.status !== 'SUCCESS' ) 
-    return Object.assign({}, state, { status: action.status })
-
+    if(action.status !== 'SUCCESS' ) 
+      return Object.assign({}, state, { status: action.status })
+  }
 
   if(action.type ==='FUNCTION_ADDED' & action.status !== 'SUCCESS' ) 
     return Object.assign({}, state, { list:[ {name:'NEW'}, ...state.list], status: action.status })
--- src/reducers/functionStore.js
+++ src/reducers/functionStore.js
@@ -1,26 +1,27 @@
 export function functionStoreReducer (state = {}, action) {
 
-  if(action.type ==='STORE_LOAD' & action.status === 'SUCCESS' ) 
-    return Object.assign({}, state, {list:action.storeList, status: action.status, error: null})
+  if(action.type ==='STORE_LOAD') {
+    if(action.status === 'SUCCESS' ) 
+      return Object.assign({}, state, {list:action.storeList, status: action.status, error: null})
 
-
-  if(action.type ==='STORE_LOAD' & action.status !== 'SUCCESS' ) 
-    return Object.assign({}, state, {status: action.status, error: action.error})
-  
+    if(action.status !== 'SUCCESS' ) 
+      return Object.assign({}, state, {status: action.status, error: action.error})
+  }
   return state
 }
 
 
-// TODO: move this to a global status instead of deploy action. 
+// TODO: move status to a global "status" reducer instead of deploy action. 
 // This current method will get hard to rationalize quickly
 export function functionDeployReducer (state = {}, action) {
 
-  if(action.type ==='FUNCTION_DEPLOY' & action.status === 'SUCCESS' ) 
-    return Object.assign({}, state, {status: action.status, error: null, message: 'Function: ' + action.functionName + ' deployed'})
+  if(action.type ==='FUNCTION_DEPLOY'){ 
+    if(action.status === 'SUCCESS' ) 
+      return Object.assign({}, state, {status: action.status, error: null, message: 'Function: ' + action.functionName + ' deployed'})
 
 
-  if(action.type ==='FUNCTION_DEPLOY' & action.status !== 'SUCCESS' ) 
-    return Object.assign({}, state, {status: action.status, error: action.error, message: null})
-  
+    if(action.status !== 'SUCCESS' ) 
+      return Object.assign({}, state, {status: action.status, error: action.error, message: null})
+  }
   return state
 }
