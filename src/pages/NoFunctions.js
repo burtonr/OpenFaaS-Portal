dfcab7fee32e99661ec77ca997a1dafe07fbfbfe
Andrew Gracey <agracey@gmail.com>
2019-07-08 08:03:45 -0700

    Revert "Added Dialog and Tabs for New Function"
    This reverts commit 70521221bc4a1b7ad34f8d16d5ac450c342be212.

--- src/pages/NoFunctions.js
+++ src/pages/NoFunctions.js
@@ -1,36 +1,8 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import StyledClosableModal from '../Presentational/StyledClosableModal';
-import NewFunctionForm from '../Presentational/NewFunctionForm';
 
-class NoFunctions extends React.Component {
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      newFunction:null
-    }
-  }
-
-  startNewFunction() {
-    this.setState({newFunction:{}})
-  }
-
-  closeNewFunction() {
-    this.setState({newFunction:null})
-  }
-
-  deployNewFunction() {
-    this.setState({newFunction:null})
-  }
-
-  handleFunctionChange(newFunction) {
-    this.setState({newFunction})
-  }
- 
-  render() {
-     
+function NoFunctions() {
     return (
         <Grid container justify="center" direction="column" alignItems="center">
             <p>
@@ -38,27 +10,15 @@ class NoFunctions extends React.Component {
                 <br />
                 Start by deploying a new function.
             </p>
-            <Button variant="contained" color="primary" onClick={this.startNewFunction.bind(this)}>
+            <Button variant="contained" color="primary">
                 Deploy New Function
             </Button>
             <p>
                 Or use faas-cli to build and deploy functions:
             </p>
             <code>$ curl -sSl https://cli.openfaas.com | sudo sh</code>
-            <StyledClosableModal 
-              open={this.state.newFunction != null}
-              handleClose={this.closeNewFunction.bind(this)}
-              title={'Deploy New Function'}
-              submitText={'Deploy'}
-            >
-                <NewFunctionForm 
-                  function={this.state.function}
-                  handleFunctionChange={this.handleFunctionChange.bind(this)}
-                />
-            </StyledClosableModal>
         </Grid>
-    );   
-  }
+    );
 }
 
 export default NoFunctions;
